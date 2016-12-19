9b2d825539e27c1c8baffb54ab92ce66f4fd0dc4
Hayase, Kazuyoshi <kazuyoshi.hayase@jcom.home.ne.jp>
2016-12-19 18:07:28 +0900

    temp

---
+++ python/pro-con-python/ch3/sec3_SegmentTree_RMQ_BIT/KthNumberBySquareSizeBucket.py
@@ -0,0 +1,18 @@
+'''
+Created on 2016/12/19
+
+@author: kazuyoshi.hayase
+'''
+B=1000 #size of bucket
+
+def solve(N,M,A,I,J,K):
+    pass
+    
+if __name__ == '__main__':
+    N=7
+    M=3
+    A=[1,5,2,6,3,7,4]
+    I=[2,4,1]
+    J=[5,4,7]
+    K=[3,1,3]
+    solve(N,M,A,I,J,K)
