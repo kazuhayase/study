9d0e4b05e1945f150c1c13515d8081af07b61109
Kazuyoshi Hayase <kazuyoshi.hayase@jcom.home.ne.jp>
2023-05-29 21:42:21 +0900

    .emacs, .emacs.d move to myconf/old create myconf/.emacs.d/init.el from leaf version

--- .emacs.d/elpa/mozc-20210306.1053/mozc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mozc" "20210306.1053" "minor mode to input Japanese with Mozc" 'nil :commit "081179512cf229c90903ef07bd409b77ea258a3b" :keywords '("mule" "multilingual" "input method"))
