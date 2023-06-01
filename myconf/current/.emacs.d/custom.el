193f310971d75dae09420bebd244ba88e4e1a7cd
Kazuyoshi Hayase <kazuyoshi.hayase@jcom.home.ne.jp>
2023-06-02 08:46:02 +0900

    hoken1-2章 WB分完了 font 設定変更

--- myconf/current/.emacs.d/custom.el
+++ myconf/current/.emacs.d/custom.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(kill-whole-line t))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "DAMA" :family "Noto Sans CJK JP")))))
