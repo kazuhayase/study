3099927b445fb33329a3e3eb7000e57175f32c68
Kazuyoshi Hayase <kazuyoshi.hayase@jcom.home.ne.jp>
2024-01-19 19:12:36 +0900

    progress index.ts

--- LLM/act-MLS2024/javascript/kazutest/src/index.ts
+++ LLM/act-MLS2024/javascript/kazutest/src/index.ts
@@ -1,10 +1,24 @@
 // import {OpenAI} from "@langchain/openai";
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 const chatModel = new ChatOpenAI({});
+const outputParser = new StringOutputParser();
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a world class technical documentation writer."],
+  ["user", "{input}"],
+]);
+const chain = prompt.pipe(chatModel);
+
+const llmChain = prompt.pipe(chatModel).pipe(outputParser);
+
 export const run = async() =>{
-       const res = await chatModel.invoke("what is LangSmith?");
-       console.log(JSON.stringify(res, null, 2));
+  // const res = await chatModel.invoke("what is LangSmith?");
+  const res = await llmChain.invoke({
+    input: "what is LangSmith?",
+  });
+  console.log(JSON.stringify(res, null, 2));
 };
 	      	
 run();		
