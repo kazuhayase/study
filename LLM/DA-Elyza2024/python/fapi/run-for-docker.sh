536c150bb7cf50e73bdee88b8f84e7b40e974893
Kazuyoshi Hayase <kazuyoshi.hayase@jcom.home.ne.jp>
2024-08-18 13:23:16 +0900

    for checking volume mount in docker-compose

--- LLM/DA-Elyza2024/python/fapi/run-for-docker.sh
+++ LLM/DA-Elyza2024/python/fapi/run-for-docker.sh
@@ -5,3 +5,6 @@
 source "/root/_home/.env"
 #uvicorn main:app --host 0.0.0.0 --port 9000 >> /tmp/rag.log 2>&1
 uvicorn main:app --host 0.0.0.0 --port 9000 >> /tmp/rag.log 
+while 1; do
+    sleep 100
+done
