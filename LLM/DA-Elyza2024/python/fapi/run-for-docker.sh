8fd98939a7d3398e4008562ff85c68c0335f226f
Kazuyoshi Hayase <kazuyoshi.hayase@jcom.home.ne.jp>
2024-08-18 10:11:07 +0900

    wait until volume mount and found .env file

--- LLM/DA-Elyza2024/python/fapi/run-for-docker.sh
+++ LLM/DA-Elyza2024/python/fapi/run-for-docker.sh
@@ -1,3 +1,6 @@
 #/bin/bash +x
-source /root/_home/.env
+    while [ ! -f "/root/_home/.env" ]; do
+	sleep 1
+    done
+source "/root/_home/.env"
 uvicorn main:app --host 0.0.0.0 --port 9000 >> /tmp/rag.log 2>&1
