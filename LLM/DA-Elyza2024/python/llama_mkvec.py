9cc0db9342f85f44c526442337c7095ac4d73a3a
Kazuyoshi Hayase <kazuyoshi.hayase@jcom.home.ne.jp>
2024-08-22 10:40:15 +0900

    update fapi and llama_index Chroma client

--- LLM/DA-Elyza2024/python/llama_mkvec.py
+++ LLM/DA-Elyza2024/python/llama_mkvec.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 
 import os
-from varlog import varlog 
+from logging import getLogger
+import inspect
+
+### https://github.com/pistatium/about_python_logging
+import json
+from logging.config import dictConfig
+
+# 設定ファイルから読み込む
+with open('logging.json') as f:
+    dictConfig(json.load(f))
+
+logger = getLogger('app')
+    
+def varlog(name):
+    # 変数が定義されているスコープを取得
+    frame = inspect.currentframe().f_back
+    variable_value = frame.f_globals.get(name) or frame.f_locals.get(name)
+    
+    if variable_value is not None:
+        logger.info(f'{name}={variable_value}')
+    else:
+        logger.warning(f'{name} is not defined')
 
 # uncomment if needs more than varlog
 # from logging import getLogger
@@ -16,57 +37,46 @@ from llama_index.core import (
     load_index_from_storage,
 )
 
-import chromadb
+from chromadb import PersistentClient 
+from chromadb import HttpClient
+from chromadb.utils import embedding_functions
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.core import Settings
+#from llama_index.core import Settings
 
 from llama_index.core.node_parser import SimpleFileNodeParser
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.extractors import TitleExtractor
 from llama_index.core.ingestion import IngestionPipeline
+from tqdm import tqdm
+
 
 required_exts = [".pdf"]
-documents = SimpleDirectoryReader(
+reader = SimpleDirectoryReader(
     "./Downloads/digital_agency_standard_guidelines/",
     recursive=True,
     required_exts=required_exts,
-).load_data(num_workers=1)
+    filename_as_id=True
+)
+documents=reader.load_data(num_workers=1)
 
 db_dir='./db_llama/'
 
-e_models=['text-embedding-ada-002', 'text-embedding-3-small', 'text-embedding-3-large']
+#e_models=['text-embedding-ada-002', 'text-embedding-3-small', 'text-embedding-3-large']
+e_models=['text-embedding-3-small']
 for e in e_models:
     varlog('e')
     db_path = f"{db_dir}/chroma_{''.join(name[0] for name in e.split('-'))}"
     varlog('db_path')
 
-    db = chromadb.PersistentClient(path=db_path)
-    chroma_collection = db.get_or_create_collection('digital_agency_standard_guidelines')
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-    embed_model = OpenAIEmbedding(model=e,tiktoken_model_name="cl100k_base")
-    service_context = ServiceContext.from_defaults(chunk_size=1000, embed_model=embed_model)
-    index = VectorStoreIndex.from_documents(
-        documents, storage_context=storage_context, service_context=service_context
+    ef = embedding_functions.OpenAIEmbeddingFunction( model_name=e, api_key=os.getenv('OPENAI_API_KEY') )
+    client = PersistentClient(path=db_path)
+    #client = HttpClient(host='localhost', port=10000)
+    collection = client.get_or_create_collection(
+        name="digital_agency_standard_guidelines",
+        embedding_function=ef
     )
-    
-    #     embed_model = embed_model,
-    #     chunk_size=1000,
-    #     vector_store = vector_store
-    # )
-    
-        # pipeline = IngestionPipeline(
-        #     transformations=[
-        #         SimpleFileNodeParser(),
-        #         #SentenceSplitter(chunk_size=25, chunk_overlap=0),
-        #         #TitleExtractor(),
-        #         embed_model,
-        #     ],
-        #     vector_store = vector_store,
-        # )
-        # pipeline.run(documents[k])
-        # index = VectorStoreIndex.from_vector_store(vector_store)
-
-        
-
+    for docs in documents:
+        collection.add(documents=docs.text,ids=docs.doc_id)
+        pass
+    pass
